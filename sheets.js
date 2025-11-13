c802cfb315f2625044eeb503123c7c18dd485af4
glom79498-create <glom79498@gmail.com>
2025-11-13 14:18:19 -0800

    Delete sheets.js

--- sheets.js
+++
@@ -1,9 +0,0 @@
-function sendToSheets(data) {
-  fetch("https://script.google.com/macros/s/AKfycbz4PLq-gNQTGo2bsJiNKpvG1DuVC69YttbwqjH3yLkKEdo6nbCVpDk65AGvbA9Nqw/exec", {
-    method: "POST",
-    body: JSON.stringify(data)
-  })
-  .then(res => res.json())
-  .then(r => console.log("Guardado:", r))
-  .catch(err => console.error("Error:", err));
-}
